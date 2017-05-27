4e806388965120cd1cc71f66f9721290c517230c
Lt19870723 <452470971@qq.com>
2017-05-27 16:35:25 +0800

    增加MQdemo

--- src/main/java/com/myproject/MySpringBootApplication.java
+++ src/main/java/com/myproject/MySpringBootApplication.java
@@ -1,13 +1,9 @@
 package com.myproject;
 
-import javax.jms.Queue;
-
-import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -20,10 +16,5 @@ public class MySpringBootApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(MySpringBootApplication.class);
-    }
-
-    @Bean
-    public Queue queue() {
-        return new ActiveMQQueue("sample.queue");
     }
 }
